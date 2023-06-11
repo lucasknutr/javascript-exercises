53fa6e6f9ecd10fabffd92864fab235def32114f
Lucas <lucasclaudinocc@gmail.com>
2023-06-11 00:59:55 -0300

    Solved 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,17 @@
-const sumAll = function() {
+const sumAll = (a, b) => {
+    if(isNaN(a) === true || isNaN(b) === true || a < 0 || b < 0) return 'ERROR';
+    let array = [];
+    if (b > a){
+        const newB = b;
+        b = a;
+        a = newB; 
+    }
+    for (i = b; i <= a; i++) {
+        array.push(i);
+    }
 
-};
+    return array.reduce((acc, num) => acc + num, 0);
+}
 
 // Do not edit below this line
 module.exports = sumAll;
